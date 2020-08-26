11948c27fa18354fdcda503272220d82667edc11
Charder001 <rick.chardet@hva.nl>
2020-08-26 17:55:26 +0200

    added code comments

--- index.js
+++ index.js
@@ -11,23 +11,28 @@ app.set("views", "views")
 app.use(express.static("public"))
 
 app.get('/', function (req, res) {
-    fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=3739d76`)
+  //fetch movie data from OMDB api using api key and imdbID
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=3739d76`)
     .then(response => {
-        return response.json();
+      return response.json();
+    })
+
+    .then(data => {
+      //render homepage with movie data from api fetch
+      res.render("home", {
+        movieInfo: data
       })
-  
-      .then(data => {
-        res.render("home", { movieInfo:data })
       console.log(data)
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 
 });
 
 http.listen(port, function () {
-    console.log(`Application started on port: ${port}`)
+  console.log(`Application started on port: ${port}`)
 })
 
+//imdbID for the fast and the furious 2001
 var imdbID = "tt0232500"
